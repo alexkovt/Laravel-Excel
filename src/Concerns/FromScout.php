c71a69d827a5a10b0cd3094245af5d0a8935be1d
Alex Kovtunov <582969@gmail.com>
2020-01-14 16:47:58 +0400

    add interactWithChunk()

--- src/Concerns/FromScout.php
+++ src/Concerns/FromScout.php
@@ -2,6 +2,7 @@
 
 namespace Maatwebsite\Excel\Concerns;
 
+use Illuminate\Database\Eloquent\Collection;
 use Laravel\Scout\Builder;
 
 interface FromScout
@@ -10,4 +11,10 @@ interface FromScout
      * @return Builder
      */
     public function scout();
+
+    /**
+     * @param iterable $chunk
+     * @return Collection
+     */
+    public function interactWithChunk($chunk);
 }
